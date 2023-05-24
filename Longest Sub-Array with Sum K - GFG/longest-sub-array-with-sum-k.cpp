b2a69fba08d9127a0623dfed5530af91f8042699
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2023-05-24 16:00:46 +0530

    Added solution - LeetHub

---
+++ Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.cpp
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int lenOfLongSubarr(int arr[],  int n, int k) 
+    { 
+    //     int i=0;
+    //     int j=0;
+    //     int ans=0;
+    //     int sum=0;
+    //   sort(a.begin(),a.end());
+    //     while(j<n){
+    //     sum=sum+a[j];
+    //         if(sum<k){
+    //             j++;
+    //         }
+    //         else if(sum==k){
+                
+    //             ans=max(ans,j-i+1); //j=i+1 is current window size
+    //             j++; //for searching next candidate
+                
+    //         }
+            
+    //         else if(sum>k){
+    //             while(sum>k){
+    //                 sum=sum-a[i];
+    //                 i++;
+    //             }
+    //         //      if(sum == k) {
+    //         //           ans=max(ans,j-i+1);
+    //         // }
+    //         // j++;
+    //         }
+    //     }
+    //     return ans;
+        // Complete the function
+         unordered_map<int, int> um;
+    int sum = 0, maxLen = 0;
+ 
+         for (int i = 0; i < n; i++) {
+ 
+        // accumulate sum
+        sum += arr[i];
+ 
+        // when subarray starts from index '0'
+        if (sum == k)
+            maxLen = i + 1;
+ 
+        // make an entry for 'sum' if it is
+        // not present in 'um'
+        if (um.find(sum) == um.end())
+            um[sum] = i;
+ 
+        // check if 'sum-k' is present in 'um'
+        // or not
+        if (um.find(sum - k) != um.end()) {
+ 
+            // update maxLength
+            if (maxLen < (i - um[sum - k]))
+                maxLen = i - um[sum - k];
+        }
+    }
+ 
+    // required maximum length
+    return maxLen;
+    } 
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	//code
+	
+	int t;cin>>t;
+	while(t--)
+	{
+	    int n, k;
+	    cin>> n >> k;
+	    int a[n];
+	    
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+	   Solution ob;
+	   cout << ob.lenOfLongSubarr(a, n , k)<< endl;
+	    
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
