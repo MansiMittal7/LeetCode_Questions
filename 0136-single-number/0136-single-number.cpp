af40f5e123295d030446017ea327aae9609f844d
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2023-06-19 11:27:47 +0530

    Time: 20 ms (74.27%), Space: 16.8 MB (69.81%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int> m;
-        for(auto i:nums) m[i]++;
-        
-       int ans=0;
-        
-        for(auto i:m){
-            if(i.second!=2) 
-             // cout<<i.first<<"\n";
-                // return m[i];
-                ans=i.first;
-        }
+        int ans=0;
+       for(int i=0; i<nums.size();i++){
+           ans=ans^nums[i];
+       }
         return ans;
     }
 };
+
+// brute force--
+    
+//      map<int,int> m;
+//         for(auto i:nums) m[i]++;
+        
+//        int ans=0;
+        
+//         for(auto i:m){
+//             if(i.second!=2) 
+//              // cout<<i.first<<"\n";
+//                 // return m[i];
+//                 ans=i.first;
+//         }
+//         return ans;
