8e283999f6ebbe41d9051458bb94770130aca10a
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2023-07-02 11:51:04 +0530

    Time: 3 ms (55.87%), Space: 8.1 MB (7.99%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     int sumOfUnique(vector<int>& nums) {
         int sum=0;
-        set<int>s;
-        map<int,int> mp;
-        for(int i=0; i<nums.size();i++) mp[nums[i]]++; 
-            // s.insert(nums[i]);
-        
-        // for(auto it=s.begin(); it!=s.end;++it) sum+=(*it);
-        // for(int i=0; i<s.size();i++) sum+=s[i];
-        
-        // for(auto it:s) mp[it]++ ;
-        
-        for(auto it:mp){
-            if(it.second==1) sum+=it.first;
-        }
-        
+       
+    multiset<int> s; 
+	for(int i = 0; i < nums.size(); i++){
+        s.insert(nums[i]);
+    }
+    for(int i = 0; i < nums.size(); i++){
+        if(s.count(nums[i]) == 1) sum += nums[i];
+    }
         return sum;
             
     }
 };
+
+//by map
+
+//map<int,int> mp;
+        // for(int i=0; i<nums.size();i++) mp[nums[i]]++; 
+        // for(auto it:mp){
+        //     if(it.second==1) sum+=it.first;
+        // }
