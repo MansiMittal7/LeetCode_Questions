a084d16a161cce8158e824269cb68fe7bd41c52c
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2023-07-22 11:04:08 +0530

    Added solution - LeetHub

---
+++ Pairwise Consecutive Elements - GFG/pairwise-consecutive-elements.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool pairWiseConsecutive(stack<int> s);
+
+// Driver program
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        stack<int> s;
+        int n, tmp;
+        cin>>n;
+        while(n--){
+            cin>>tmp;
+	        s.push(tmp);
+	    }
+	    if (pairWiseConsecutive(s))cout << "Yes" << endl;
+	    else cout << "No" << endl;
+	    /*while (s.empty() == false)
+        {
+            cout << s.top() << " ";
+            s.pop();
+        }
+        cout<<endl;*/
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+// your task is to complete the function
+// function should return true/false or 1/0
+bool pairWiseConsecutive(stack<int> s)
+{
+    //Code here
+    
+   stack <int> a;
+   a.push(s.top());
+   s.pop();
+   
+   while(!s.empty()){
+       
+       if(a.top() - s.top() !=1) return false;
+       
+       a.pop();
+       a.push(s.top());
+       s.pop();
+       
+   }
+   return true;
+}
