6be99e54cde16bbe00a0be26d402689ad4a0b83a
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2023-08-29 12:16:59 +0530

    Added solution - LeetHub

--- Rotation - GFG/rotation.cpp
+++ Rotation - GFG/rotation.cpp
@@ -9,45 +9,23 @@ using namespace std;
 class Solution{
 public:	
 	int findKRotation(int arr[], int n) {
-	    // code here
-	    int low = 0, high = n-1;
-	    int index=-1;
-	    int ans=INT_MAX;
 	    
-	    while(low<=high){
-	        int mid =(high +low)/2;
-	        
-	        if(arr[low]<= arr[high]){
-	            if(arr[low]<ans){
-	                index=low;
-	                ans= arr[low];
-	            }
-	           break;
-	        }
-	        
-	        if (arr[low]<= arr[mid]){
-	            if(arr[low]<ans){
-	                index=low;
-	                ans= arr[low];
-	            }
-	            low = mid+1;
-	        }
-	        
-	        else{
-	            high = mid-1;
-	            if(arr[mid]<ans){
-	                index=mid;
-	                ans= arr[mid];
-	                
-	            } 
-	        }
-	        
+	    //binary search O(logn)
+	    //index of smallest number = ans 
+	    //smallest number lies in unsorted part 
+	    
+	    int i=0,j=n-1;
+	    int cnt=0;
+	    while(arr[i]>arr[j]){
+	        i++;
+	        cnt++;
 	    }
-	    
-	    return index;
+	    return cnt;
 	}
 
 };
+
+//linear search O(n)
 
 //{ Driver Code Starts.
 
