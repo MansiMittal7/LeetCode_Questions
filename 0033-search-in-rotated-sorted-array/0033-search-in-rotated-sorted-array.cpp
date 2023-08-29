6a8254e34e4557fd954a7c462dcf2c5738a4359f
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2023-08-29 12:51:54 +0530

    Time: 3 ms (68.50%), Space: 11 MB (59.79%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,15 +1,49 @@
 class Solution {
+private:
+   int minIdx(vector<int>&v)
+    {
+        int n = size(v);
+        int l = 0, h = n-1, md;
+        if(v[l] <= v[h]) return 0;
+        while(h >= l){
+            md = l + (h-l)/2;
+            int prev = (md-1+n)%n;
+            int next = (md+1)%n;
+            if((v[md] < v[prev]) && (v[md] < v[next])) return md;
+            if(v[md] >= v[0]) l = md + 1;
+            else h = md - 1;
+        }
+        return 0;
+    }
+    int bs(vector<int>&v,int st,int ed,int k)
+    {
+        int l = st, h = ed, md;
+        while(h >= l){
+            md = l + (h-l)/2;
+            if(v[md] == k) return md;
+            else if(v[md] > k) h = md - 1;
+            else l = md + 1;
+        }
+        return -1;
+    }
 public:
     int search(vector<int>& nums, int target) {
-        
-        // vector<int> v;
-        int ans=-1;
-        for(int i=0; i<nums.size(); i++){
-           if(nums[i]==target) ans=i;
-               // return {i};
-            // else return {-1};
+        int idx = minIdx(nums), n = size(nums);
+        int st = idx, ed = n-1;
+        int ans = bs(nums,st,ed,target);
+        if(ans == -1){
+            st = 0, ed = idx-1;
+            ans = bs(nums,st,ed,target);
         }
         return ans;
-       
     }
 };
+        
+        //liner Search O(n)
+        // int ans=-1;
+        // for(int i=0; i<nums.size(); i++){
+        //    if(nums[i]==target) ans=i;
+        //        // return {i};
+        //     // else return {-1};
+        // }
+        // return ans;
