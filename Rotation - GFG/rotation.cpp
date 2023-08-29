256549dc2062916f363e441b2fdc9894a0512d5f
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2023-08-29 12:20:57 +0530

    Added solution - LeetHub

--- Rotation - GFG/rotation.cpp
+++ Rotation - GFG/rotation.cpp
@@ -14,18 +14,36 @@ public:
 	    //index of smallest number = ans 
 	    //smallest number lies in unsorted part 
 	    
-	    int i=0,j=n-1;
-	    int cnt=0;
-	    while(arr[i]>arr[j]){
-	        i++;
-	        cnt++;
+	    int s = 0;
+	    int e = n-1;
+	     
+	    while(s<e)
+	    {
+	        int mid = s+(e-s)/2;
+	        if(arr[mid]<arr[e]) //second part sorted
+	        {
+	            e = mid;
+	        } else //first part sorted
+	        {
+	            s = mid+1;
+	        }
+	        
 	    }
-	    return cnt;
+	    return e;
+	    
+	   
 	}
 
 };
 
 //linear search O(n)
+//int i=0,j=n-1;
+	   // int cnt=0;
+	   // while(arr[i]>arr[j]){
+	   //     i++;
+	   //     cnt++;
+	   // }
+	   // return cnt;
 
 //{ Driver Code Starts.
 
