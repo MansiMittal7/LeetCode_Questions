fa61b9df86d45746bdf211345c3bcab77fcf444a
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2023-09-02 22:58:02 +0530

    Time: 40 ms (64.11%), Space: 19 MB (47.42%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -10,63 +10,67 @@ class Solution {
 // }
 
 
-// int calculateTotalHours(vector<int> &v, int hourly) {
-//     int totalH = 0;
-//     int n = v.size();
-//     //find total hours:
-//     for (int i = 0; i < n; i++) {
-//         totalH += ceil((double)(v[i]) / (double)(hourly));
-//     }
-//     return totalH;
-// }
-    
-// public:
-//     int minEatingSpeed(vector<int>& v, int h) {
-//       int low = 1, high = findMax(v);
-
-//       while (low <= high) {
-//         int mid = (low + high) / 2;
-//         int totalH = calculateTotalHours(v, mid);
-//         if (totalH <= h) {
-//             high = mid - 1;
-//         }
-//         else {
-//             low = mid + 1;
-//         }
-//     }
-//     return low;
-    
-
-    int calHours(vector<int>& piles, int k)
-    {
-        int sum=0; 
-        for (auto p: piles)
-        {
-            sum+=p/k;
-            if (p%k!=0)
-                sum++;
-        }
-        return sum; 
+long long int calculateTotalHours(vector<int> &v, int hourly) {
+   long long int totalH = 0;
+    int n = v.size();
+    //find total hours:
+    for (int i = 0; i < n; i++) {
+        totalH += ceil((double)(v[i]) / (double)(hourly));
     }
+    return totalH;
+}
+    
 public:
-    int minEatingSpeed(vector<int>& piles, int H) {
-        // left and right bounds for how many to eat
-        // find smallest k so:  f(k)<=H 
-        // smallest k,  more hours (largest hours) <=H
-        int left=1, right=0; 
-        for (auto p: piles)  // max value is the max size of the piles.
-            right= max(right , p);
-        
-        while (left <right)
-        {
-            int mid=left+ (right-left)/2;
-            int f_mid= calHours(piles, mid);
-            if (f_mid<=H)  //meeting the condition, we want to keep it
-                right = mid;  // we want to have larger mid, search smaller K
-            else // H<f_mid , k too small, exceeding hours, search larger half
-                left=mid+1; 
+   long long int minEatingSpeed(vector<int>& v, int h) {
+      long long int low = 1;
+      // int high = findMax(v);
+        long long int high  = *max_element(v.begin(),v.end());
+
+      while (low <= high) {
+        long long int mid = low + (high-low) / 2;
+         long long int totalH = calculateTotalHours(v, mid);
+        if (totalH <= h) {
+            high = mid - 1;
         }
+        else {
+            low = mid + 1;
+        }
+    }
+    return low;
+    
+    
+    //second alternative solution 
+
+//     int calHours(vector<int>& piles, int k)
+//     {
+//         int sum=0; 
+//         for (auto p: piles)
+//         {
+//             sum+=p/k;
+//             if (p%k!=0)
+//                 sum++;
+//         }
+//         return sum; 
+//     }
+// public:
+//     int minEatingSpeed(vector<int>& piles, int H) {
+//         // left and right bounds for how many to eat
+//         // find smallest k so:  f(k)<=H 
+//         // smallest k,  more hours (largest hours) <=H
+//         int left=1, right=0; 
+//         for (auto p: piles)  // max value is the max size of the piles.
+//             right= max(right , p);
         
-        return left; 
+//         while (left <right)
+//         {
+//             int mid=left+ (right-left)/2;
+//             int f_mid= calHours(piles, mid);
+//             if (f_mid<=H)  //meeting the condition, we want to keep it
+//                 right = mid;  // we want to have larger mid, search smaller K
+//             else // H<f_mid , k too small, exceeding hours, search larger half
+//                 left=mid+1; 
+//         }
+        
+//         return left; 
     }
 };
