75f0204c214ce961ec99a4094a86f535212e0a2c
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2023-09-03 12:49:37 +0530

    Added solution - LeetHub

--- Aggressive Cows - GFG/aggressive-cows.cpp
+++ Aggressive Cows - GFG/aggressive-cows.cpp
@@ -8,46 +8,38 @@ using namespace std;
 // User function Template for C++
 
 class Solution {
-public:
-
-    bool ispossiblesolution(int n , int k , vector<int> &stalls, int mid){
-                int count = 1;
-                int pos = stalls[0];
-                for(int i = 0; i<stalls.size(); i++){
-                    if(stalls[i] - pos  >=  mid){
-                        count++;
-                        pos = stalls[i];
-                        
-                        
-                    }
-                    if(count == k)
-                        return true;
-                }
-                return false;
+    
+    
+    bool CanWePlace(vector<int> &stalls, int dist, int cows){
+      int cntCows=1, last=stalls[0];
+        for(int i=1; i<stalls.size();i++){
+            if(stalls[i]-last>=dist){
+                cntCows++;
+                last=stalls[i];
             }
-
- 
+            if(cntCows>=cows) return true;
+        }
+        
+        return false;
+    }
+    
+public:
 
     int solve(int n, int k, vector<int> &stalls) {
     
-       sort(stalls.begin(), stalls.end());
-        int start = 0;
-        int end = stalls[stalls.size()-1] - stalls[0];
-        int ans = -1;
+        // Write your code here
+        sort(stalls.begin(),stalls.end());
+        int low=0, high = stalls[n-1]-stalls[0];
         
-        while(start<=end){
-            int mid = (start+end)/2;
-            if(ispossiblesolution(n, k, stalls, mid)){
-                ans = mid;
-                start = mid+1;
-            }
-            else{
-                end = mid-1;
-            }
+        while(low<=high){
+            int mid=(low+high)/2;
             
+            if(CanWePlace(stalls,mid,k)==true) low=mid+1;
             
+            else high=mid-1;
         }
-        return ans;
+        
+        return high;
     }
 };
 
