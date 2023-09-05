f2477cd158f21e9e1f5b0452a3f2c6969807f1ba
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2023-09-05 20:03:28 +0530

    Added solution - LeetHub

--- Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..cpp
+++ Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..cpp
@@ -31,52 +31,104 @@ struct Node *start = NULL;
 */
 class Solution
 {
+     void InsertAtTail (Node* &tail, Node* curr){
+            tail->next =curr;
+            tail=curr;
+        }
+        
+        
     public:
     //Function to sort a linked list of 0s, 1s and 2s.
     Node* segregate(Node *head) {
         
         // Add code here
-        // brute force TC: O(N)+O(N)=O(N)   SC:O(1)
-    
-        int onecount =0, twocount=0, zerocount=0 ;
+        // when data cant be replaced then we have to sort by changing the linkings 
         
-        Node* temp = head;
         
-        while(temp!=NULL){
-            if(temp->data == 0) zerocount++;
-            
-            else if(temp->data == 1) onecount++;
-            
-            else twocount++;
-            
-            temp=temp->next;
-        }
+       Node *zeroHead = new Node(-1);
+       Node *zeroTail = zeroHead;
+       Node *oneHead = new Node(-1);
+       Node *oneTail = oneHead;
+       Node *twoHead = new Node(-1);
+       Node *twoTail = twoHead;
+       
+       Node *curr=head;
+       
+       //create seperate lists of 0,1 and 2
+       while(curr!=NULL){
+           
+           int value= curr-> data;
+           
+           if(value==0) InsertAtTail(zeroTail, curr);
+           
+           else if(value==1) InsertAtTail(oneTail, curr);
+           
+           else if(value==2) InsertAtTail(twoTail, curr);
+           
+           curr=curr->next;
+       }
         
-        temp = head;
+        //merge 3 sublists
         
-        while(temp!=NULL){
-            
-            if(zerocount!=0) {
-                temp->data = 0;
-                zerocount--;
-            }
-            
-            else if(onecount!=0) {
-                temp->data = 1;
-                onecount--;
-            }
-            
-            else if(twocount!=0) {
-                temp->data = 2;
-                twocount--;
-            }
-            temp=temp->next;
-        }
+        //1's list isnt empty
+        if(oneHead->next!=NULL) zeroTail->next = oneHead->next;
         
+        //1's list is empty
+        else zeroTail->next = twoHead->next;
+        
+        oneTail->next= twoHead->next;
+        twoTail->next = NULL;
+        
+        head=zeroHead->next; //setup head
+        
+        delete zeroHead; //deleting dummy node 
+        delete oneHead;
+        delete twoHead;
+            
         return head;
-        
     }
 };
+
+
+// brute force TC: O(N)+O(N)=O(N)   SC:O(1)
+    
+        // int onecount =0, twocount=0, zerocount=0 ;
+        
+        // Node* temp = head;
+        
+        // while(temp!=NULL){
+        //     if(temp->data == 0) zerocount++;
+            
+        //     else if(temp->data == 1) onecount++;
+            
+        //     else twocount++;
+            
+        //     temp=temp->next;
+        // }
+        
+        // temp = head;
+        
+        // while(temp!=NULL){
+            
+        //     if(zerocount!=0) {
+        //         temp->data = 0;
+        //         zerocount--;
+        //     }
+            
+        //     else if(onecount!=0) {
+        //         temp->data = 1;
+        //         onecount--;
+        //     }
+            
+        //     else if(twocount!=0) {
+        //         temp->data = 2;
+        //         twocount--;
+        //     }
+        //     temp=temp->next;
+        // }
+        
+        // return head;
+        
 
 
 //{ Driver Code Starts.
