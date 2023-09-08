9a16d35410fb744213a1471588feec787e6bc21e
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2023-09-08 12:13:37 +0530

    Time: 0 ms (100.00%), Space: 10.8 MB (19.21%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,37 +11,53 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-       
-         if(head==NULL){
-         return head;
-         }   
-        ListNode* temp=head;
-        int count=0;
-        while(temp!=NULL){
-            count++;
-            temp=temp->next;
+       //optimised approach 
+        ListNode* fast = head;
+        ListNode* slow = head;
+        for(int i=0;i<n;++i){
+                fast=fast->next;            
         }
-        temp=head;
-        
-        if(count==n) return head->next;
-        
-        
-        int pos=count-n+1; 
-        
-           count=1;
-     while(count<pos-1&&temp->next!=NULL){
-         count++;
-         temp=temp->next;
-    }
-     ListNode* todelete=temp->next;
-    
-     temp->next=temp->next->next;
-    delete todelete;
-    return head;
-            
-        
+        if (fast == nullptr){
+            return head->next;
+        }
+        while(fast->next){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        ListNode* temp = slow->next;
+        slow->next = slow->next->next;
+        delete temp;
+        return head;
         
     }
 };
-     //brute force count length of list, subtract n from it, then remove that node from list
+    //brute force count length of list, subtract n from it, then remove that node from list
    // tc=O(2*N) sc=O(1)
+  //  if(head==NULL){
+ //          return head;
+//          }   
+//         ListNode* temp=head;
+//         int count=0;
+//         while(temp!=NULL){
+//             count++;
+//             temp=temp->next;
+//         }
+//         temp=head;
+        
+//         if(count==n) return head->next;
+        
+        
+//         int pos=count-n+1; 
+        
+//            count=1;
+//      while(count<pos-1&&temp->next!=NULL){
+//          count++;
+//          temp=temp->next;
+//     }
+//      ListNode* todelete=temp->next;
+    
+//      temp->next=temp->next->next;
+//     delete todelete;
+//     return head;
+            
+        
