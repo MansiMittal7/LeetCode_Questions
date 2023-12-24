db25659429b5055490f43ec2e1d7c9966bc3e1bc
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2023-12-24 17:18:03 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,52 +3,131 @@
 using namespace std;
 
 // } Driver Code Ends
+
+
+class DisjointSet{
+    vector<int> rank,parent,size;
+public:
+    DisjointSet(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0; i<=n;i++){
+            parent[i]=i;
+            size[i]=1;
+        }   
+    }
+    
+    int findUPar(int node){
+        if(node==parent[node]) return node;
+        return parent[node]=findUPar(parent[node]);
+    }
+    
+    void unionByRank(int u,int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u==ulp_v) return;
+        if(rank[ulp_u]<rank[ulp_v]) parent[ulp_u]=ulp_v;
+        else if (rank[ulp_v]<rank[ulp_u]) parent[ulp_v]=ulp_u;
+        else {
+            parent[ulp_v]=ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    
+    void unionBySize(int u,int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u==ulp_v) return;
+        if(size[ulp_u]<size[ulp_v]){
+            parent[ulp_u]=v;
+            size[ulp_v]+=size[ulp_u];
+        }
+        else{
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+    }
+
+};
+
 class Solution
 {
+    
+    //by kruskal algo-- 
+    
+    
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-        priority_queue<pair<int,int>, 
-        vector<pair<int,int>>, greater<pair<int,int>>> pq;
         
-        vector<int> vis(V,0);
-        //{wt,node}
-        pq.push({0,0});
-        int sum=0;
-        //ElogE + ElogE == ElogE
-        
-        while(!pq.empty()){
-           //logE
-           auto it=pq.top();
-           pq.pop();
-           int node= it.second;
-           int wt= it.first;
-           
-           if(vis[node] == 1) continue;
-           //add it to the mst
-           
-           vis[node]=1;
-           sum+=wt;
-           //ElogE
-           
-           for(auto it: adj[node]){
-               int adjNode = it[0];
-               int edW = it[1];
-               
-               if(!vis[adjNode]){
-                   pq.push({edW,adjNode});
-               }
-               
-           }
-           
+        vector<pair<int,pair<int,int>>>edges;
+        for(int i=0;i<V;i++){  //going across each edge
+            for(auto it:adj[i]){
+                int adjnode=it[0];
+                int wt=it[1];
+                int node=i;
+                
+                edges.push_back({wt,{node,adjnode}});
+            }
+        }
+        DisjointSet ds(V);
+        sort(edges.begin(), edges.end());
+        int mstweight=0;
+        for(auto it: edges){
+            int wt=it.first;
+            int u=it.second.first;
+            int v=it.second.second;
+            
+            if(ds.findUPar(u) != ds.findUPar(v)){
+               mstweight+=wt;
+               ds.unionBySize(u,v);
+            }
+            
         }
         
-        return sum;
+        return mstweight;
         
     }
 };
+
+
+
+//by prims algo-
+  
+// 	public:
+// 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+//     int spanningTree(int V, vector<vector<int>> adj[])
+//     {
+//         priority_queue<pair<int,int>, 
+//         vector<pair<int,int>>, greater<pair<int,int>>> pq;
+//         vector<int> vis(V,0);
+//         //{wt,node}
+//         pq.push({0,0});
+//         int sum=0;
+//         //ElogE + ElogE == ElogE
+//         while(!pq.empty()){
+//           //logE
+//           auto it=pq.top();
+//           pq.pop();
+//           int node= it.second;
+//           int wt= it.first;
+//           if(vis[node] == 1) continue;
+//           //add it to the mst
+//           vis[node]=1;
+//           sum+=wt;
+//           //ElogE
+//           for(auto it: adj[node]){
+//               int adjNode = it[0];
+//               int edW = it[1];
+//               if(!vis[adjNode]){
+//                   pq.push({edW,adjNode});
+//               }
+//           }
+//         }
+//         return sum;
 
 //{ Driver Code Starts.
 
