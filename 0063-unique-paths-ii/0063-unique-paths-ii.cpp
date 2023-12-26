7d35710928331683a9ffd53421feb7445ed33649
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2023-12-26 21:04:00 +0530

    Time: 2 ms (60.41%), Space: 7.8 MB (93.53%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,73 @@
+class Solution {
+//tabulation methond 
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int mod=(2e9+7);
+        int n=obstacleGrid.size(); //nXm matrix
+        int m=obstacleGrid[0].size();
+        
+        int dp[n][m];
+        for(int i=0; i<n;i++){
+            for(int j=0;j<m;j++){
+                if(obstacleGrid[i][j]==1) dp[i][j]=0;
+                else if(i==0 && j==0) dp[i][j]=1;
+                
+                else{
+                    int up=0, left=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    
+                    dp[i][j]=(up+left)%mod;
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+};
+
+//recursion methond 
+// private:
+//     int f(int i,int j,vector<vector<int>>& obstacleGrid){
+//         if(i>=0 && j>=0 && obstacleGrid[i][j]==-1) //defining base case
+//         if(i==0 && j==0) return 1;
+//         if(i<0 || j<0) return 0;
+        
+//         int up=f(i-1,j,obstacleGrid);
+//         int left=f(i,j-1,obstacleGrid);
+        
+//         return up+left;
+//     }
+// public:
+//     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        
+//         int n=obstacleGrid.size(); //nXm matrix
+//         int m=obstacleGrid[0].size();
+        
+        
+//         return f(n-1,m-1,obstacleGrid,dp);
+//     }
+
+
+
+//memoization methond 
+// private:
+//     int f(int i,int j,vector<vector<int>>& obstacleGrid, vector<vector<int>> & dp){
+//         if(i>=0 && j>=0 && obstacleGrid[i][j]==-1) //defining base cases
+//         if(i==0 && j==0) return 1;
+//         if(i<0 || j<0) return 0;
+//         if(dp[i][j]!=-1) return dp[i][j];
+        
+//         int up=f(i-1,j,obstacleGrid,dp);
+//         int left=f(i,j-1,obstacleGrid,dp);
+        
+//         return dp[i][j]=up+left; //storing before returning  
+//     }
+// public:
+//     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        
+//         int n=obstacleGrid.size(); //nXm matrix
+//         int m=obstacleGrid[0].size();
+//         vector<vector<int>> dp(n,vector<int>(m,-1));
+        
+//         return f(n-1,m-1,obstacleGrid,dp);
+//     }
