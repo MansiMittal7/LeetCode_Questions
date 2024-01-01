7ca195dd1345586196b65304f390b4f32589daa3
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2024-01-01 13:30:34 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -6,27 +6,27 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
-    //tabulation approach  
-    
+    //space optimization 
     public:
     //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int maxweight, int wt[], int val[], int n) 
     { 
        // Your code here
-       vector<vector<int>> dp(n,vector<int>(maxweight+1 , 0));
-       for(int w=wt[0]; w<=maxweight; w++) dp[0][w]=val[0];
+       vector<int>prev(maxweight+1 , 0), curr(maxweight+1 , 0);
+       
+       for(int w=wt[0]; w<=maxweight; w++) prev[w]=val[0];
        
        for(int ind=1; ind<n;ind++){
            for(int w=0;w<=maxweight; w++){
-               int notTake=0+dp[ind-1][w];
+               int notTake=0+prev[w];
                int take=INT_MIN;
-               if(wt[ind]<=w) take=val[ind]+dp[ind-1][w-wt[ind]];
-               dp[ind][w]= max(take,notTake);
+               if(wt[ind]<=w) take=val[ind]+prev[w-wt[ind]];
+               curr[w]= max(take,notTake);
            }
-           
+           prev=curr;
        }
        
-       return dp[n-1][maxweight];
+       return prev[maxweight];
     }
 };
 
@@ -59,6 +59,28 @@ class Solution
     //   // Your code here
     //   vector<vector<int>> dp(n,vector<int>(maxweight+1 , -1));
     //   return f(n-1, maxweight, wt, val,dp);
+    
+    
+ //tabulation approach  
+//  public:
+//     //Function to return max value that can be put in knapsack of capacity W.
+//     int knapSack(int maxweight, int wt[], int val[], int n) 
+//     { 
+//       // Your code here
+//       vector<vector<int>> dp(n,vector<int>(maxweight+1 , 0));
+//       for(int w=wt[0]; w<=maxweight; w++) dp[0][w]=val[0];
+       
+//       for(int ind=1; ind<n;ind++){
+//           for(int w=0;w<=maxweight; w++){
+//               int notTake=0+dp[ind-1][w];
+//               int take=INT_MIN;
+//               if(wt[ind]<=w) take=val[ind]+dp[ind-1][w-wt[ind]];
+//               dp[ind][w]= max(take,notTake);
+//           }
+           
+//       }
+       
+//       return dp[n-1][maxweight];
     
 
 
