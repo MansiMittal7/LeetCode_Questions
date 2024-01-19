f98b765b17aebd5eb94d234e51def0e33a62925a
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2024-01-19 11:52:48 +0530

    Time: 8 ms (76.82%), Space: 9.4 MB (32.19%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,24 +1,24 @@
 class Solution {
-//space optimization 
+// tabulation 
 public:
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
         
-        vector<int> prev(m+1,0), curr(m+1,0);
+        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
         
-        for(int j=0;j<=m;j++) prev[j]=j;
+        for(int i=0;i<=n;i++) dp[i][0]=i;
+        for(int j=0;j<=m;j++) dp[0][j]=j;
         
         for(int i=1;i<=n;i++){
-              curr[0]=i;
             for(int j=1;j<=m;j++){
-             if(word1[i-1]==word2[j-1]) curr[j] = prev[j-1];
-              else curr[j] = 1+min( prev[j] , min( curr[j-1], prev[j-1] ) );   
-            }
-          prev=curr;
+              if(word1[i-1]==word2[j-1]) dp[i][j] = dp[i-1][j-1];
+              else dp[i][j] = 1+min( dp[i-1][j] , min( dp[i][j-1], dp[i-1][j-1] ) );
+                
+            } 
         } 
 
-        return prev[m];
+        return dp[n][m];
     }
 };
 
@@ -90,3 +90,24 @@ public:
 //         } 
 
 //         return dp[n][m];
+
+//space optimization 
+// public:
+//     int minDistance(string word1, string word2) {
+//         int n=word1.size();
+//         int m=word2.size();
+        
+//         vector<int> prev(m+1,0), curr(m+1,0);
+        
+//         for(int j=0;j<=m;j++) prev[j]=j;
+        
+//         for(int i=1;i<=n;i++){
+//               curr[0]=i;
+//             for(int j=1;j<=m;j++){
+//              if(word1[i-1]==word2[j-1]) curr[j] = prev[j-1];
+//               else curr[j] = 1+min( prev[j] , min( curr[j-1], prev[j-1] ) );   
+//             }
+//           prev=curr;
+//         } 
+
+//         return prev[m];
