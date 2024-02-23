ea26365909693eb47f04482d631fc8d786dcaaf8
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2024-02-23 18:01:39 +0530

    Time: 8 ms (96.85%), Space: 22.6 MB (5.27%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,24 +1,45 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-  
+      // approach with one vectors-
+        int n=nums.size();
         vector<int> v1;
-        vector<int> v2;
-     for(int i=0; i<nums.size(); i++){
+        for(int i=0; i<n; i++){
          if(nums[i]!=0) v1.push_back(nums[i]);
-         else v2.push_back(nums[i]);
-     }
-        
-        
-        for(int i=0; i<v1.size(); i++){
-            nums[i]=v1[i];
         }
-        for(int i=0; i<v2.size(); i++){
-            nums[i+v1.size()]=v2[i];
-        }
-     // return nums;
+        
+        int nz=v1.size();
+        
+        for (int i = 0; i < nz; i++) nums[i] = v1[i];
+
+        for (int i = nz; i < n; i++) nums[i] = 0;
+    
     }
 };
+
+
+
+
+//approach with two vectors-
+//         vector<int> v1;
+//         vector<int> v2;
+//      for(int i=0; i<nums.size(); i++){
+//          if(nums[i]!=0) v1.push_back(nums[i]);
+//          else v2.push_back(nums[i]);
+//      }
+        
+        
+//         for(int i=0; i<v1.size(); i++){
+//             nums[i]=v1[i];
+//         }
+//         for(int i=0; i<v2.size(); i++){
+//             nums[i+v1.size()]=v2[i];
+//         }
+
+
+
+
+
 
  //  int i=nums.size()-1;
 //         for(int j=nums.size()-1;j<=0; --j ){
