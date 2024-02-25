f51a0d25ff32e9d012bfa28852db005b0edd6b36
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2024-02-25 16:10:25 +0530

    Time: 20 ms (44.73%), Space: 12.8 MB (31.40%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,48 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+     
+    int n=s.size();
+    int i=0, j=0, ans=0;
+    map<char,int> mp;
         
-        vector<int> map(256,-1);
-        int left=0, right=0;
-        int n=s.size();
-        int len=0;
-        while(right<n){
-            if(map[s[right]]!=-1) left=max(map[s[right]]+1, left);
-            map[s[right]]=right;
-            len=max(len,right-left+1);
-            right++;
+    //all unique char are needed so size of current window== unique char count size of map
+    while(j<n){
+        mp[s[j]]++;
+        if(mp.size()>j-i+1){
+            j++;
         }
-
-        return len;
+        else if(mp.size()==j-i+1){
+            ans=max(ans,j-i+1);
+            j++;
+        }
+        else if(mp.size()<j-i+1){ //repeating chars present in map  
+            while(mp.size()<j-i+1){
+              mp[s[i]]--;
+              if(mp[s[i]]==0) mp.erase(s[i]);
+              i++;
+            }
+            j++;
+        }
+    }
+    return ans;
+        
     }
 };
+
+
+
+
+
+// vector<int> map(256,-1);
+//         int left=0, right=0;
+//         int n=s.size();
+//         int len=0;
+//         while(right<n){
+//             if(map[s[right]]!=-1) left=max(map[s[right]]+1, left);
+//             map[s[right]]=right;
+//             len=max(len,right-left+1);
+//             right++;
+//         }
+
+//         return len;
