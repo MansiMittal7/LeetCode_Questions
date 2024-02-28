b0ba8c21f85224dc16c992046b290f660acf1e30
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2024-02-28 12:46:37 +0530

    Time: 13 ms (27.41%), Space: 8.5 MB (53.90%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int hash1[27]={0}, hash2[27]={0};
-        for (int i=0; i<s.size(); i++)
-            hash1[s[i]-'a']++;
-        for (int i=0; i<t.size(); i++)
-            hash2[t[i]-'a']++;
-        bool flag=true;
-        for (int i=0; i<26; i++){
-            if(hash1[i]!=hash2[i]) return false;
+        if (s.size()!=t.size()) return false;
+        
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        
+        // int cnt=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=t[i]) {
+                return false;
+                break;
+            }
+            // else cnt++;
         }
-        return flag;    
+        // if(cnt==s.size()) 
+            return true;
     }
 };
+
+//using hash 
+// int hash1[27]={0}, hash2[27]={0};
+//         for (int i=0; i<s.size(); i++)
+//             hash1[s[i]-'a']++;
+//         for (int i=0; i<t.size(); i++)
+//             hash2[t[i]-'a']++;
+//         bool flag=true;
+//         for (int i=0; i<26; i++){
+//             if(hash1[i]!=hash2[i]) return false;
+//         }
+//         return flag;    
