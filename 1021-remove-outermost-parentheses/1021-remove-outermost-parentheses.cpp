1dbe3f91b0d97f4718421021946fb1ec2063ed60
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2024-03-02 13:30:17 +0530

    Time: 31 ms (5.40%), Space: 177.9 MB (5.45%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -2,21 +2,42 @@ class Solution {
 public:
     string removeOuterParentheses(string s) {
         string ans="";
-        int open=0, j=0;
-        for(int i=0; i<s.size();i++){
-            if(s[i]=='(')open++;
-            else open--;
-            if(open==0){
-                ans=ans+s.substr(j+1,i-j-1);
-                    j=i+1;
+        int n=s.size();
+        stack<char> st;
+        
+        for(int i=0;i<n;i++){
+            if(s[i]=='(') {
+               if(!st.empty()){
+                   ans=ans+s[i]; //current bracket outermost nhi hai 
+               } 
+                st.push(s[i]); 
+               
+            }
+            
+            else{
+                st.pop();
+                if(!st.empty()){
+                    ans=ans+s[i];
+                }
+                
             }
         }
+        
         return ans;
+        
     }
 };
-// else if (s[i] == ')')
-//                 c--;
-//             if (!c) {
-//                 s1 += s.substr(j + 1, i - j - 1);
-//                 j = i + 1;
-//             }
+
+
+
+ // string ans="";
+ //        int open=0, j=0;
+ //        for(int i=0; i<s.size();i++){
+ //            if(s[i]=='(')open++;
+ //            else open--;
+ //            if(open==0){
+ //                ans=ans+s.substr(j+1,i-j-1);
+ //                    j=i+1;
+ //            }
+ //        }
+ //        return ans;
