4aff224305716410def50d47d5a111422bca6b94
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2024-03-09 20:30:01 +0530

    Time: 101 ms (25.23%), Space: 69.2 MB (12.38%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,8 +1,20 @@
 class Solution {
-public:
-    int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        int ans=-1, n1=nums1.size(),n2=nums2.size();
-        
+    
+ public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2){
+      // using pre computation technique 
+     unordered_set<int> set1(nums1.begin(),nums1.end());
+     for(auto it:nums2){
+         if(set1.contains(it)) return it;
+     }
+     return -1;
+    }
+};
+
+
+
+// int ans=-1, n1=nums1.size(),n2=nums2.size();
+        //brute force
         // for(int i=0;i<n1;i++){
         //     for(int j=0;j<n2;j++){
         //         if(nums1[i]==nums2[j]){
@@ -13,15 +25,16 @@ public:
         // }
         // return ans;
         
-        
-        int i=0, j=0;
-        while(i<nums1.size() && j<nums2.size()) {
+
+
+
+        //two pointer
+//         int i=0, j=0;
+//         while(i<nums1.size() && j<nums2.size()) {
             
-            if(nums1[i]<nums2[j]) i++;
-            else if(nums1[i]>nums2[j]) j++;
-            else if(nums1[i] == nums2[j]) return nums1[i];
+//             if(nums1[i]<nums2[j]) i++;
+//             else if(nums1[i]>nums2[j]) j++;
+//             else if(nums1[i] == nums2[j]) return nums1[i];
             
-        }
-        return -1;
-    }
-};
+//         }
+//         return -1;
