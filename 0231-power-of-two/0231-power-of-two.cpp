c421103291631a559cd32906a243ff14f9033f5a
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2024-03-11 21:21:10 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (73.72%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
+
     bool isPowerOfTwo(int n) {
-        if(n<=0) return false;
-      if(n & (n-1)) return false;
-        else return true;
-        
+        if(n==0) return false;
+        while(n%2==0) n=n/2;
+        return n==1;
     }
 };
-// while(n!=1){
-        // n=n/2;
-        // c++;
-        // }
 
-// brute force
-//   int c=0,check=1,flag=0;
-//         if(n==1) return true;
-//         else if(n%2!=0 && n!=1) return false;
-        
-//         while(n%2==0){
-//             n=n/2;
-//             if(n==1){
-//                 flag=1;
-//                 break;
-//             }
-//         }
-        
-//         if(flag==0) return false;
-//         else return true;
+// bit manupulation 
+    //     if(n<=0) return false;
+    //   if(n & (n-1)) return false;
+    //     else return true;
