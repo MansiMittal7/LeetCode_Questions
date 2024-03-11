bbcf7f215140fbca08d87c4b72f3b6e8996c709d
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2024-03-11 14:40:16 +0530

    Time: 3 ms (50.27%), Space: 8.2 MB (30.10%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
+      
+        unordered_map<char,int> freq;
+        for(auto it:s) freq[it]++;
         
-        auto cmp = [&](char a, char b){
-             return order.find(a)<order.find(b);
-        };
-            
+        string ans="";
         
-        sort(s.begin(),s.end(),cmp);
-        return s;
+        for(auto it:order){
+            while(freq[it]-- > 0) ans.push_back(it);
+        }
+        
+        for(auto [el,fr]: freq){
+            while(fr-- > 0) ans.push_back(el);
+        }
+        return ans;
     }
 };
+
+
+// auto cmp = [&](char a, char b){
+//              return order.find(a)<order.find(b);
+//         };
+            
+//         sort(s.begin(),s.end(),cmp);
+//         return s;
