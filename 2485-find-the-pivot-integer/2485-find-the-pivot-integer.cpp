397286c322ae4c33f0f523559369699ec7b46c21
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2024-03-13 15:06:39 +0530

    Time: 3 ms (51.39%), Space: 7.3 MB (41.86%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        vector<int> v;
-        for(int i=0;i<n;i++) v.push_back(i+1);
-        
-        int start=0, end=n-1,bsum=v[0],esum=v[n-1],ans=-1;
-        
-        while(end>=start){
-            
-            if(bsum==esum && start==end){
-                ans=start+1;
-                break;
-            }
-            else if(bsum<esum) {
-                start++;
-                bsum+=v[start];
-            }
-                
-            
-            else {
-                 end--;
-                 esum+=v[end];
-            }
-                
-        }
-        return ans;
+     //using maths- solving the equality 
+        int sum = (n*(n+1)/2);
+        int x=sqrt(sum);
+        // return x*x=sum? x : -1
+        if(x*x == sum ) return x;
+        else return -1;
         
     }
 };
+
+
+
+        //two pointer 
+//         vector<int> v;
+//         for(int i=0;i<n;i++) v.push_back(i+1);
+        
+//         int start=0, end=n-1,bsum=v[0],esum=v[n-1],ans=-1;
+        
+//         while(end>=start){ 
+//             if(bsum==esum && start==end){
+//                 ans=start+1;
+//                 break;
+//             }
+//             else if(bsum<esum) {
+//                 start++;
+//                 bsum+=v[start];
+//             }
+//             else {
+//                  end--;
+//                  esum+=v[end];
+//             }      
+//         }
+//         return ans;
