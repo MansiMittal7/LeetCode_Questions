db0fb3bed64a13219cd753568c16d122df6156cc
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2024-03-14 12:55:49 +0530

    Time: 19 ms (87.16%), Space: 30.9 MB (94.04%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,22 +1,56 @@
 class Solution {
+private:
+    int atMost(vector<int>& nums, int g) {
+        if (g < 0) return 0;
+        int n = nums.size();
+        
+        int res = 0, i = 0,j=0;
+        for (int j = 0; j < n; j++) {
+            g -= nums[j];
+            while (g < 0){
+               g += nums[i];
+               i++;  
+            }
+               
+            res += j - i + 1;
+        }
+        
+        // while(i<j){
+        //     g-=nums[j];
+        //     while(g<0){
+        //         g+=nums[i];
+        //         i++;
+        //     }
+        //         res+=j-i+1;
+        //     j++;
+        // }
+        return res;
+    }
+    
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
     
-      // return atMost(nums,goal)-atMost(nums,goal-1);
-      int ans=0, preSum=0;
-      unordered_map<int,int> freq;
-        freq[0]=1;
-        
-        for(auto it:nums ){
-            preSum+=it;
-            if(freq.find(preSum-goal)!=freq.end())
-                ans+=freq[preSum-goal];
-            freq[preSum]++;
-        }
-        return ans;
+      return atMost(nums,goal)-atMost(nums,goal-1);
+      // sub with sum=goal = subarray with sum<=goal  -  subarray with sum < goal
+     
     }
  
 };
+
+
+//  int ans=0, preSum=0;
+//       unordered_map<int,int> freq;
+//         freq[0]=1;
+        
+//         for(auto it:nums ){
+//             preSum+=it;
+//             if(freq.find(preSum-goal)!=freq.end())
+//                 ans+=freq[preSum-goal];
+//             freq[preSum]++;
+//         }
+//         return ans;
+
+
 
 //map<int,int> mp;
 //         mp[0] = 1;
