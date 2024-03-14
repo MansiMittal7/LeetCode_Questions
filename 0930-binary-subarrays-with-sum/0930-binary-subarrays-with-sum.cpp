c72b1bf41ff933be0938f70881d01df87fc178f2
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2024-03-14 12:11:07 +0530

    Time: 39 ms (52.67%), Space: 38.3 MB (29.75%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -2,53 +2,21 @@ class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
     
-     return atMost(nums,goal)-atMost(nums,goal-1);
-    }
-private:
-    int atMost(vector<int>& nums, int goal){
-        int n=nums.size(), i=0,j=0,ans=0,sum=0;
-        if(goal<0) return 0;
-         while(j<n){
-            sum+=nums[j];
-            
-//             if(sum<=goal){
-//                 ans+=(j-i+1);
-//                 j++;
-//             }
-//             else{
-//                 while(sum>goal){
-//                     sum-=nums[i];
-//                     i++;
-//                 }
-//                 j++;
-//             }
-             
-            while(sum>goal)
-            {
-                sum-=nums[i];
-                i++;
-            }
-
-            ans+=(j-i+1); 
-            j++;
+      // return atMost(nums,goal)-atMost(nums,goal-1);
+      int ans=0, preSum=0;
+      unordered_map<int,int> freq;
+        freq[0]=1;
+        
+        for(auto it:nums ){
+            preSum+=it;
+            if(freq.find(preSum-goal)!=freq.end())
+                ans+=freq[preSum-goal];
+            freq[preSum]++;
         }
         return ans;
     }
-
+ 
 };
-
-
-//     int atMost(vector<int>& A, int S) {
-//         if (S < 0) return 0;
-//         int res = 0, i = 0, n = A.size();
-//         for (int j = 0; j < n; j++) {
-//             S -= A[j];
-//             while (S < 0)
-//                 S += A[i++];
-//             res += j - i + 1;
-//         }
-//         return res;
-
 
 //map<int,int> mp;
 //         mp[0] = 1;
@@ -57,8 +25,6 @@ private:
 //             preSum+=nums[i];
 //             int remove= preSum-goal;
 //             cnt+=mp[remove];
-//             mp[remove]+=1;
-            
-//         }
-        
+//             mp[remove]+=1;          
+//         }      
 //         return cnt;
