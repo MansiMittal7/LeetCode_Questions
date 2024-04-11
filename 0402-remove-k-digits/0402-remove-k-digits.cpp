c299ab51a49a522040e6ab1befc5808041037094
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2024-04-11 13:35:33 +0530

    Time: 4 ms (97.62%), Space: 10.3 MB (26.88%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -2,24 +2,61 @@ class Solution {
 public:
     string removeKdigits(string num, int k) {
         
-       string res = "";
-        stack<char> st;
+        int n=num.size();
+        stack<char>st;
         
-        for(char ch : num)
-        {
-            while(!res.empty() && ch < res.back() && k > 0)
-            {
-                res.pop_back();
+        for(int i=0; i<n; i++){ //removing large digits before small digits
+            while(!st.empty()  && k>0  &&  st.top()>num[i]){
+                st.pop();
                 k--;
             }
-            if(!res.empty() || ch != '0') res.push_back(ch);
+//             if(!st.empty()  && num[i] != '0') st.push(num[i]);
+                st.push(num[i]);
         }
         
-        while(!res.empty() && k--)
-            res.pop_back();
+        //removing last k digits
+        while(k>0 && !st.empty()){
+            st.pop();
+            k--;
+        }
         
-        if(res == "")
-            res = "0";
-        return res;
+        string ans="";//forming string 
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        
+        int endIndex=0;//finding index to eliminate zeros
+        for(int i=ans.size()-1; i>=0;i--){
+            if(ans[i]!='0'){
+                endIndex=i;
+                break;
+            }
+        }
+        
+        ans=ans.substr(0,endIndex+1);
+        reverse(ans.begin(),ans.end());
+        if(ans=="") ans="0";//edge case
+        
+        return ans;
     }
 };
+
+
+
+
+        // removing largest value from stack
+//         while(!st.empty() && k--){
+//             st.pop();
+            
+//             if(st.empty()) return 0;
+//         }
+        
+//         //retrieving the num formed
+        
+//         while(!st.empty()){
+//             num[n-1]=st.top();
+//             st.pop();
+//             n=n-1;
+//         }
+//         return num.substr(n);
