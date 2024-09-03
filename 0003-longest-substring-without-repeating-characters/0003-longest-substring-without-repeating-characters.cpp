7453f6a3f3b049d2ef03ab3c60c6843039a3bb61
Mansi Mittal <96714095+MansiMittal7@users.noreply.github.com>
2024-09-03 17:29:58 +0530

    Time: 32 ms (13.91%), Space: 14.4 MB (33.53%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,48 +1,30 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-    //variable sliding window 
-    int n=s.size();
-    int i=0, j=0, ans=0;
-    map<char,int> mp;
+        int n=s.length();
+        int maxLenth = 0;
         
-    //all unique char are needed so size of current window== unique char count size of map
-    while(j<n){
-        mp[s[j]]++;
-        // if(mp.size()>j-i+1){//this conditions ccan never occur 
-        //     j++;
-        // }
+        unordered_set<char> charSet;
         
-        if(mp.size()==j-i+1){ //all unique chars present in map 
-            ans=max(ans,j-i+1);
-            j++;
-        }
-        else if(mp.size()<j-i+1){ //repeating chars present in map  
-            while(mp.size()<j-i+1){
-              mp[s[i]]--;
-              if(mp[s[i]]==0) mp.erase(s[i]);
-              i++;
+        int left=0;
+        
+        for(int right =0; right<n; right++){
+          
+            if(charSet.count( s[right])==0){
+                charSet.insert(s[right]);
+                cout<<right-left+1;
+                maxLenth = max(maxLenth, right-left+1);
+            } //checking for multiple resp
+            else {
+                while(charSet.count(s[right])){
+                    charSet.erase(s[left]);
+                    left++;
+                }
+                charSet.insert(s[right]);
             }
-            j++;
+            
         }
-    }
-    return ans;
+        return maxLenth;
         
     }
 };
-
-
-
-
-
-// vector<int> map(256,-1);
-//         int left=0, right=0;
-//         int n=s.size();
-//         int len=0;
-//         while(right<n){
-//             if(map[s[right]]!=-1) left=max(map[s[right]]+1, left);
-//             map[s[right]]=right;
-//             len=max(len,right-left+1);
-//             right++;
-//         }
-//         return len;
